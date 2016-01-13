4649bfd489b906446b6665d02fe935782b5d9d3f
Ly Nguyen <Ly900@aol.com>
2016-01-13 16:15:48 -0500

    retrieve json data from server

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ var app = express();
 // Automatically goes to the public folder and uses the index.html there.
 app.use(express.static(__dirname + "/public"));
 
+// Sends the problems array as json data if a request is made to this path
+app.get("/problems", function(req, res) {
+  console.log("Server received a get request");
+
+  problem1 = {
+    question: "What is a variable?",
+    answer: "A variable is a container that stores information.",
+    resource_links:  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types"
+  };
+
+  problem2 = {
+    question: "How would you organize your Javascript code?",
+    answer: "",
+    resource_links: "http://read.humanjavascript.com/ch04-organizing-your-code.html"
+  };
+
+  problem3 = {
+    question: "What is an IIFE and why is it used?",
+    answer: "",
+    resource_links:
+      ["http://stackoverflow.com/questions/8228281/what-is-the-function-construct-in-javascript", "http://lucybain.com/blog/2014/immediately-invoked-function-expression/"]
+  };
+  var problems = [problem1, problem2, problem3];
+  res.json(problems);
+
+}); //ends app.get
+
+
+
 app.listen(3000, function(){
 console.log("Server running on port 3000");
 });
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,57 +4,26 @@ var myApp = angular
     '$scope',
     '$http',
     function($scope, $http) {
-    console.log("Response from Angular controller");
-    $scope.cardClass = "question-card-front";
+      console.log("Response from Angular controller");
+      // $scope.cardClass = "question-card-front";
+      // $scope.changeClass = function() {
+      //   if ($scope.cardClass == "question-card-front") {
+      //     $scope.cardClass = "question-card-back";
+      //   } else {
+      //     $scope.cardClass = "question-card-front";
+      //   }
+      // }; //ends changeClass function
 
-    $scope.changeClass = function() {
-      if ($scope.cardClass == "question-card-front") {
-        $scope.cardClass = "question-card-back";
-      } else {
-        $scope.cardClass = "question-card-front";
-      }
-    }; //ends changeClass function
+      // This tells the front-end to get the data from the back-end with path "/problem"; once it comes back,
+      $http.get("/problems")
+      .success(function(response) {
+        console.log("I got the data I requested");
+        console.log(response);
+        $scope.problems = response;
+      }); // ends .success method
 
-    problem1 = {
-      question: "What is a variable?",
-      answer: "A variable is a container that stores information.",
-      resource_links:  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types"
-    };
 
-    problem2 = {
-      question: "How would you organize your Javascript code?",
-      answer: "",
-      resource_links: "http://read.humanjavascript.com/ch04-organizing-your-code.html"
-    };
 
-    problem3 = {
-      question: "What is an IIFE and why is it used?",
-      answer: "",
-      resource_links:
-        ["http://stackoverflow.com/questions/8228281/what-is-the-function-construct-in-javascript", "http://lucybain.com/blog/2014/immediately-invoked-function-expression/"]
-    };
-
-    problem4 = {
-      question: "What is a variable?",
-      answer: "A variable is a container that stores information.",
-      resource_links:  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types"
-    };
-
-    problem5 = {
-      question: "How would you organize your Javascript code?",
-      answer: "",
-      resource_links: "http://read.humanjavascript.com/ch04-organizing-your-code.html"
-    };
-
-    problem6 = {
-      question: "What is an IIFE and why is it used?",
-      answer: "",
-      resource_links:
-        ["http://stackoverflow.com/questions/8228281/what-is-the-function-construct-in-javascript", "http://lucybain.com/blog/2014/immediately-invoked-function-expression/"]
-    };
-
-    var problems = [problem1, problem2, problem3, problem4, problem5, problem6];
-    $scope.problems = problems;
 
 
 
