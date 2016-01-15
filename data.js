d7e75ac0bf9dd2f96071ee937ef5e370d047dfeb
Ly Nguyen <Ly900@aol.com>
2016-01-15 09:03:21 -0500

    add more questions

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 db.techwizme.insert([
-  {
+  problem1 = {
     question: "What is a variable?",
     resource_links:  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types"
   },
@@ -26,5 +26,40 @@ db.techwizme.insert([
     question: "What is refactoring? Why and how do we refactor?",
     resource_links:
       "https://sourcemaking.com/refactoring/extract-method"
+  },
+  problem7 = {
+    question: "Why do we use 'use strict' at the beginning of a Javascript source file?",
+    resource_links:
+      "http://www.toptal.com/javascript/interview-questions"
+  },
+  problem8 = {
+    question: "What is a closure in Javascript? Provide an example.",
+    resource_links:
+      "http://www.toptal.com/javascript/interview-questions"
+  },
+  problem9 = {
+    question: "What is the difference between undefined and not defined in JavaScript?",
+    resource_links:
+      "https://www.codementor.io/javascript/tutorial/21-essential-javascript-tech-interview-practice-questions-answers"
+  },
+  problem10 = {
+    question: "What is the drawback of creating true private methods in JavaScript?",
+    resource_links:
+      "https://www.codementor.io/javascript/tutorial/21-essential-javascript-tech-interview-practice-questions-answers"
+  },
+  problem11 = {
+    question: "Name at least 2 methods for emptying an array.",
+    resource_links:
+      "https://www.codementor.io/javascript/tutorial/21-essential-javascript-tech-interview-practice-questions-answers"
+  },
+  problem12 = {
+    question: "How do you check if an object is an array or not?",
+    resource_links:
+      "https://www.codementor.io/javascript/tutorial/21-essential-javascript-tech-interview-practice-questions-answers"
+  },
+  problem13 = {
+    question: "What is function hoisting?",
+    resource_links:
+      "https://www.codementor.io/javascript/tutorial/21-essential-javascript-tech-interview-practice-questions-answers"
   }
 ])
